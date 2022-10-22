a34c41bd0db2f946545291c3d29cece1949d9a7a
Steven <gonghaima@hotmail.com>
2022-10-22 20:20:11 +1100

    projects page

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -20,7 +20,7 @@ const Skill = ({ directionLeft }: Props) => {
         xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
       />
       <div
-        className="absolute opacity-0 group-hover:opacity-70 transition duration-300
+        className="absolute opacity-0 group-hover:opacity-60 transition duration-300
       ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32
       rounded-full z-0"
       >
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Header from '../components/Header';
 import Hero from '../components/Hero';
 import WorkExperience from '../components/WorkExperience';
 import Skills from '../components/Skills';
+import Projects from '../components/Projects';
 
 const Home: NextPage = () => {
   return (
@@ -27,7 +28,9 @@ const Home: NextPage = () => {
       <section id="skills" className="snap-start">
         <Skills />
       </section>
-      {/** Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
       {/** Contact Me */}
     </div>
   );
